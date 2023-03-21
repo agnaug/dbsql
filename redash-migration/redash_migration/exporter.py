91317ba8bf8cf7f861ae2eac93fa6985d1fb4e5d
Agnieszka Augustyniak <agnieszka.augustyniak@databricks.com>
2023-03-22 09:57:21 +1100

    Update project scaffolding

---
+++ redash-migration/redash_migration/exporter.py
@@ -0,0 +1,19 @@
+# Standard Library
+import datetime
+import json
+import os
+
+# Third Party
+import requests
+
+
+def export_query():
+    pass
+
+
+def export_dashboard():
+    pass
+
+
+def export_alert():
+    pass
--- redash-migration/redash_migration/importer.py
+++ redash-migration/redash_migration/importer.py
@@ -6,3 +6,15 @@ import os
 # Third Party
 import requests
 from redash_toolbelt.client import Redash
+
+
+def import_query():
+    pass
+
+
+def import_dashboard():
+    pass
+
+
+def import_alert():
+    pass
