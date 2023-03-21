ff45eeb28099d2f4b236c875aeee4fd69e8af4c3
Agnieszka Augustyniak <agnieszka.augustyniak@databricks.com>
2023-03-22 10:30:37 +1100

    Add more scaffolding

--- redash-migration/redash_migration/main.py
+++ redash-migration/redash_migration/main.py
@@ -1,2 +1,26 @@
-def main():
-    pass
+# Our Libraries
+from redash_migration.args import parse_args
+
+
+def main() -> None:
+    args = parse_args()
+
+    # Create Redash Client
+
+    # Run import from Redash Workspace
+
+    # Download Dashboards - by dashboard last usage date
+
+    # Download Queries - by dashboard last usage or/and query last usage date
+
+    # Download Alerts
+
+    # Run transformations on queries
+
+    # Run export to Databricks SQL
+
+    # Export Queries
+
+    # Export Dashboards
+
+    # Export Alerts
